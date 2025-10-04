3b4ef3919be4be915a8c2e5209ad1d42b8876142
bd2025-creator <baron.dyer@yahoo.com>
2025-10-04 01:30:13 -0400

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        canary: {
+          yellow: '#FFE566',
+          beak: '#FFB703',
+          sky: '#E6F4FF'
+        }
+      }
+    }
+  },
+  plugins: []
+}
